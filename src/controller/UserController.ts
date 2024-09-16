0392738bee20213f5da2826a65de8c3155e0f8e2
fasal <fasal>
2024-09-16 11:41:29 +0530

    feat: create order API

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -4,12 +4,33 @@ import {Controller, Req, Res, Post, UseBefore, Get} from "routing-controllers";
 import { Request, Response } from "express";
 import ORDER_SERVICE from "../services/user/order";
 
-@Controller("/order")
+@Controller("/user")
 export class UserController{
-    @Get("/list")
+    @Get("/menu-list")
     async menuList(@Req() req: Request, @Res() res: Response) {
         try {
             let { status, data } = await new ORDER_SERVICE().getMenuList({ STATUS: true })
+            return res
+                .status(status)
+                .json(data);
+
+        } catch (err: any) {
+            console.log(err);
+            if (err.name = 'ZodError') {
+                let i = err.issues[err.issues.length-1]
+                return res
+                    .status(400)
+                    .json({ msg: i.message || 'something went wrong' });
+            } else
+                return res
+                    .status(404)
+                    .json({ msg: 'something went wrong' });
+        }
+    }
+    @Post("/order")
+    async book(@Req() req: Request, @Res() res: Response) {
+        try {
+            let { status, data } = await new ORDER_SERVICE().createOrder(req?.body)
             return res
                 .status(status)
                 .json(data);
--- src/services/user/order.ts
+++ src/services/user/order.ts
@@ -5,9 +5,18 @@ export default class Order {
     constructor(){
         this.mongo = new MongoCRUD()
     }
-    public async createMenu(doc:any) {
-        let menuItem = await this.mongo.save('Menu',doc)
-        if (menuItem?.MENU_ID) return {status:201,data:{status:201,message:'successfully added menu'}}
+    public async createOrder(doc:any) {
+        const payload = {
+            USER_ID: "USER005",
+            STATUS : "Pending",
+            ...doc
+        }
+        await this.updateItmsWithPrice(payload?.ITEMS)
+        payload.AMOUNT = payload.ITEMS.reduce((acc: number,obj: any) => {
+            return acc + obj.TOTAL
+        }, 0)
+        let menuItem = await this.mongo.save('Order',payload)
+        if (menuItem?.ORDER_ID) return {status:201,data:{status:201,message:'order booked Successfully'}}
         throw new Error('error while saving data')
     }
     public async getMenuList(doc:any) {
@@ -17,12 +26,27 @@ export default class Order {
     }
     public async getMenuById(doc:any) {
         const response = await this.mongo.findOne('Menu',doc)
-        if(response?._doc?.MENU_ID) return {status:200,data:{status: 200,data: response._doc}}
+        if(response?._doc?.MENU_ID) return {status:200,data:{status: 200,data: (response._doc)}}
         throw new Error('error while getting data')
     }
     public async updateMenu(filter:any,doc: any) {
         const response = await this.mongo.findOneAndUpdate('Menu',filter,doc)
         if (response?._doc?.MENU_ID) return {status:200,data:{status: 200,message:'successfully updated menu'}}
         throw new Error('error while updating data')
+    }
+    public async updateItmsWithPrice(items: any) {
+        for (let i = 0; i < items.length; i++) {
+            try {
+              const response = await this.getMenuById({ MENU_ID: items[i].MENU_ID });
+              if(response?.status == 200){
+                items[i].PRICE = response?.data?.data?.PRICE
+                items[i].TOTAL = response?.data?.data?.PRICE * items[i].QUANTITY
+              }
+            } catch (error) {
+              console.error(`Failed to fetch data for item ${items[i].id}:`, error);
+              throw new Error(`Failed to fetch data for item ${items[i].id}:`)
+            }
+        }
+        return 'OK'
     }
 }
